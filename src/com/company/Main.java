b75504acab8f1be1e72d7b51a1ddb4aac59c6c5f
Andrew McClary <andrew.mccalry00@gmail.com>
2018-01-25 16:31:06 -0500

    Fizzbuzz

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,36 @@
 package com.company;
-
+import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        // write your code here
+        Scanner input = new Scanner(System.in);
+
+        int userNumber = 1;
+
+        do {
+            if ((userNumber % 3 == 0) && (userNumber % 5 == 0)){
+                System.out.println("fizzbuzz");
+            }
+            else if (userNumber % 5 == 0){
+                System.out.println("buzz");
+            }
+            else if (userNumber % 3 == 0){
+                System.out.println("fizz");
+            }
+            else {
+                System.out.println(userNumber);
+            }
+            userNumber += 1;
+        }
+        while(userNumber <= 100);
+
+        
     }
 }
+/*
+Fizzbuzz - Take user input of 1 - 100
+if it is a multiple of 3 output "fizz"
+if it is a multiple of 5 output "buzz"
+if it is a multiple of both output " fizzbuzz"
+jf jt js a multiple of neither output its value
+ */
