6d31ce0a187b7baeff968c7377c34a4c2ff8ba32
Andrew McClary <andrew.mccalry00@gmail.com>
2018-01-26 13:56:46 -0500

    Grading problem

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,26 +5,63 @@ public class Main {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
-        int userNumber = 1;
-
-        do {
-            if ((userNumber % 3 == 0) && (userNumber % 5 == 0)){
-                System.out.println("fizzbuzz");
-            }
-            else if (userNumber % 5 == 0){
-                System.out.println("buzz");
-            }
-            else if (userNumber % 3 == 0){
-                System.out.println("fizz");
-            }
-            else {
-                System.out.println(userNumber);
-            }
-            userNumber += 1;
+//        int userNumber = 1;
+//
+//        do {
+//            if ((userNumber % 3 == 0) && (userNumber % 5 == 0)){
+//                System.out.println("Fizz Buzz");
+//            }
+//            else if (userNumber % 5 == 0){
+//                System.out.println("Buzz");
+//            }
+//            else if (userNumber % 3 == 0){
+//                System.out.println("Fizz");
+//            }
+//            else {
+//                System.out.println(userNumber);
+//            }
+//            userNumber += 1;
+//        }
+//        while(userNumber <= 100);
+//
+//        for (int i = 1; i <= 100 ; i++){
+//            if(i % 3 == 0 && i % 5 == 0){
+//                System.out.println("Fizz Buzz");
+//            }
+//            else if (i % 3 == 0){
+//                System.out.println("Fizz");
+//            }
+//            else if (i % 5 == 0){
+//                System.out.println("Buzz");
+//            }
+//            else {
+//                System.out.println(i);
+//            }
+//        }
+        /*
+        a. Below 45 - F
+        b. 45 to 49 - D
+        c. 50 to 59 - C
+        d. 60 to 79 - B
+        e. Above 80 - A
+         */
+        System.out.println("What is your grade in class?");
+        int grade = input.nextInt();
+        if (grade < 45){
+            System.out.println("You have an F.");
         }
-        while(userNumber <= 100);
-
-        
+        else if (grade >= 45 && grade <= 49){
+            System.out.println("You have a D.");
+        }
+        else if (grade >= 50 && grade <= 59){
+            System.out.println("You have a C.");
+        }
+        else if (grade >= 60 && grade <= 79){
+            System.out.println("You have a B.");
+        }
+        else{
+            System.out.println("You have an A");
+        }
     }
 }
 /*
